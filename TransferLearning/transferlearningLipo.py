e955be013804d4c67087d17a3df3a0ac0fc17eb5
Gustav <gustavlagneborg@hotmail.com>
2022-03-21 12:14:42 +0100

    changes to repo

--- TransferLearning/transferlearningLipo.py
+++ TransferLearning/transferlearningLipo.py
@@ -39,31 +39,31 @@ tf.random.set_seed(
 )
 
 loadPath = "SavedModels/SmilesColor/"
-path = "SavedModels/SmilesColorLipo/"
+path = "../SavedModels/SmilesColorLipo/"
 modelName = "Transfer learning"
 batch_size = 32
-nb_epoch = 100
+nb_epoch = 200
 verbose = 1
 
 # change depending on image, 180 for mol images, 0 for others
 rotation_range = 0
 
 params = {
-    'conv1_units': 64,
-    'conv2_units': 64,
-    'conv3_units': 64,
-    'conv4_units': 64,
-    'conv5_units': 64,
-    'conv6_units': 64,
-    'num_block1': 1,
-    'num_block2': 1,
-    'num_block3': 1,
+    'conv1_units': 16,
+    'conv2_units': 16,
+    'conv3_units': 16,
+    'conv4_units': 16,
+    'conv5_units': 16,
+    'conv6_units': 16,
+    'num_block1': 3,
+    'num_block2': 3,
+    'num_block3': 3,
     'dropval': 0.5,
 }
 
 # preprocessing
-train_file_exists = os.path.exists('LipophilicityData/df_train_preprocessed.csv')
-test_file_exists = os.path.exists('LipophilicityData/df_test_preprocessed.csv')
+train_file_exists = os.path.exists('../LipophilicityData/df_train_preprocessed.csv')
+test_file_exists = os.path.exists('../LipophilicityData/df_test_preprocessed.csv')
 
 if (train_file_exists == False and test_file_exists == False):
     # get dataset
@@ -115,27 +115,27 @@ if (train_file_exists == False and test_file_exists == False):
     testing_data = testing_data.reset_index(drop=True)
 
     # Save preprocessed test and train dataset
-    training_data.to_csv(path_or_buf="LipophilicityData/df_train_preprocessed.csv")
-    testing_data.to_csv(path_or_buf="LipophilicityData/df_test_preprocessed.csv")
+    training_data.to_csv(path_or_buf="../LipophilicityData/df_train_preprocessed.csv")
+    testing_data.to_csv(path_or_buf="../LipophilicityData/df_test_preprocessed.csv")
 else:
     print("Found local preprocessed dataframe")
-    training_data = pd.read_csv("LipophilicityData/df_train_preprocessed.csv", index_col=[0]).reset_index(drop=True)
-    testing_data = pd.read_csv("LipophilicityData/df_test_preprocessed.csv", index_col=[0]).reset_index(drop=True)
+    training_data = pd.read_csv("../LipophilicityData/df_train_preprocessed.csv", index_col=[0]).reset_index(drop=True)
+    testing_data = pd.read_csv("../LipophilicityData/df_test_preprocessed.csv", index_col=[0]).reset_index(drop=True)
 
 # image creation
 imageType = "SmilesColor"
 
 if imageType == "SmilesColor":
-    for fname in os.listdir("LipophilicityImages/SmilesColorImages/Train"):
+    for fname in os.listdir("../LipophilicityImages/SmilesColorImages/Train"):
         if fname.endswith('.png'):
             print("png files already exists in specified path - Smiles Color LipophilicityImages")
             break
     else:
         # train images
-        generateImageSMILEColor(path="LipophilicityImages/SmilesColorImages/Train/", compoundList=training_data, HIV_activity="regression", withChars=False)
+        generateImageSMILEColor(path="../LipophilicityImages/SmilesColorImages/Train/", compoundList=training_data, HIV_activity="regression", withChars=False)
 
         # test images
-        generateImageSMILEColor(path="LipophilicityImages/SmilesColorImages/Test/", compoundList=testing_data, HIV_activity="regression", withChars=False)
+        generateImageSMILEColor(path="../LipophilicityImages/SmilesColorImages/Test/", compoundList=testing_data, HIV_activity="regression", withChars=False)
 
 # Transfer learning
 DirTrainImg = "LipophilicityImages/SmilesColorImages/Train/"
@@ -143,12 +143,12 @@ DirTestImg = "LipophilicityImages/SmilesColorImages/Test/"
 DirTensorArray = "LipophilicityData/SmilesColorArray/"
 
 # Loading trainig LipophilicityData
-if os.path.exists("LipophilicityData/SmilesColorArray/X_Train.pickle"):
+if os.path.exists("../LipophilicityData/SmilesColorArray/X_Train.pickle"):
     print("Local train LipophilicityData was found" + "\n")
-    pickle_in = open("LipophilicityData/SmilesColorArray/X_train.pickle", "rb")
+    pickle_in = open("../LipophilicityData/SmilesColorArray/X_Train.pickle", "rb")
     X_train_and_valid = pickle.load(pickle_in)
 
-    pickle_in = open("LipophilicityData/SmilesColorArray/y_Train.pickle", "rb")
+    pickle_in = open("../LipophilicityData/SmilesColorArray/y_Train.pickle", "rb")
     y_train_and_valid = pickle.load(pickle_in)
 
 else:
@@ -157,12 +157,12 @@ else:
     print("Done!")
 
     # Loading individual test LipophilicityData
-if os.path.exists("LipophilicityData/SmilesColorArray/X_Test.pickle"):
+if os.path.exists("../LipophilicityData/SmilesColorArray/X_Test.pickle"):
     print("Local test LipophilicityData was found" + "\n")
-    pickle_in = open("LipophilicityData/SmilesColorArray/X_Test.pickle", "rb")
+    pickle_in = open("../LipophilicityData/SmilesColorArray/X_Test.pickle", "rb")
     X_test = pickle.load(pickle_in)
 
-    pickle_in = open("LipophilicityData/SmilesColorArray/y_Test.pickle", "rb")
+    pickle_in = open("../LipophilicityData/SmilesColorArray/y_Test.pickle", "rb")
     y_test = pickle.load(pickle_in)
 
 else:
@@ -204,7 +204,7 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         tf.config.experimental.set_virtual_device_configuration(gpus[0], [
-            tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10444)])
+            tf.config.experimental.VirtualDeviceConfiguration(memory_limit=6082)])
     except RuntimeError as e:
         print(e)
 
@@ -221,7 +221,7 @@ results = pd.DataFrame(columns=['Train Loss', 'Validation Loss', 'Test Loss', 'T
 
 # load pretrained model
 filecp = loadPath + "_bestweights_trial_" + ".hdf5"
-model, submodel = cs_setup_cnn(params, inshape=input_shape, classes=1, lr=0.00001)
+model, submodel = cs_setup_cnn(params, inshape=input_shape, classes=1, lr=0.001)
 submodel.load_weights(filecp, by_name=True)
 
 for layer in submodel.layers:
@@ -231,7 +231,7 @@ tf_model = Sequential()
 tf_model.add(submodel)
 tf_model.add(Dense(units=1, activation='linear'))
 
-optimizer = RMSprop(lr=0.0001)
+optimizer = RMSprop(lr=0.001)
 tf_model.compile(optimizer=optimizer, loss="mean_squared_error")
 
 print(tf_model.summary())
@@ -246,33 +246,32 @@ callbacks = [TerminateOnNaN(),
              CSVLogger(filecsv)]
 
 # Train model
-with tf.device('/cpu:0'):
-    datagen = ImageDataGenerator()
-    hist = tf_model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size),
-                                  epochs=nb_epoch, steps_per_epoch=X_train.shape[0] / batch_size,
-                                  verbose=verbose,
-                                  validation_data=(X_valid, y_valid),
-                                  callbacks=callbacks)
 
-    # Visualize loss curve
-    hist_df = cs_keras_to_seaborn(hist)
-    cs_make_plots(hist_df, filename=path)
+datagen = ImageDataGenerator()
+hist = tf_model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size),
+                              epochs=nb_epoch, steps_per_epoch=X_train.shape[0] / batch_size,
+                              verbose=verbose,
+                              validation_data=(X_valid, y_valid),
+                              callbacks=callbacks)
 
-    # Save model and history
-    hist = hist.history
-    tf_model.save(path + modelName)
-    pickle_out = open(path + modelName + "_History" + ".pickle", "wb")
-    pickle.dump(hist, pickle_out)
-    pickle_out.close()
+# Visualize loss curve
+hist_df = cs_keras_to_seaborn(hist)
+cs_make_plots(hist_df, filename=path)
 
-with tf.device('/cpu:0'):
-    # Reload best model & compute results
-    tf_model.load_weights(filecp)
-    cs_compute_results(tf_model, classes=1, df_out=results,
-                       train_data=(X_train, y_train),
-                       valid_data=(X_valid, y_valid),
-                       test_data=(X_test, y_test),
-                       filename=path)
+# Save model and history
+hist = hist.history
+tf_model.save(path + modelName)
+pickle_out = open(path + modelName + "_History" + ".pickle", "wb")
+pickle.dump(hist, pickle_out)
+pickle_out.close()
+
+# Reload best model & compute results
+tf_model.load_weights(filecp)
+cs_compute_results(tf_model, classes=1, df_out=results,
+                   train_data=(X_train, y_train),
+                   valid_data=(X_valid, y_valid),
+                   test_data=(X_test, y_test),
+                   filename=path)
 
 # Calculate results for entire CV
 final_mean = results.mean(axis=0)
